bddcc8ff57927e6da4e59f1dde2c410af286bcc2
djangify <djangify@gmail.com>
2025-06-16 17:41:10 +0100

    Update checkout process

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,8 +6,10 @@ interface ImportMetaEnv {
   readonly PUBLIC_JWT_REFRESH_TOKEN_LIFETIME: string;
   readonly PUBLIC_EMAIL_VERIFICATION_URL: string;
   readonly PUBLIC_PASSWORD_RESET_URL: string;
+  readonly PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
