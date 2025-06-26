51f52747e5da88c946743c07304867e81ec1db96
djangify <djangify@gmail.com>
2025-06-26 23:16:07 +0100

    Update ecommerce page and homepage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      // This completely replaces Tailwind's default scale
+      'base': '1.125rem', // Previously 1rem  
+      'lg': '1.25rem',   // Previously 1.125rem
+      'xl': '1.5rem',    // Previously 1.25rem
+
+    },
     extend: {
       fontFamily: {
         sans: ['Merriweather', 'ui-sans-serif', 'system-ui', 'sans-serif'],
       },
     },
-
   },
   plugins: [
     require('@tailwindcss/typography'),
